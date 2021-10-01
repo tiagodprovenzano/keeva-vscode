c552f30d0c03104f86ed41eef7fd131f60ac21b5
Tiago <31420080+tiagodprovenzano@users.noreply.github.com>
2021-10-01 14:46:32 -0300

    creates a folder with component files

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {Component} from '@tiagodprovenzano/react-keeva/'
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -12,10 +13,28 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('react-keeva-vs.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('react-keeva-vs.helloWorld', (uri: vscode.Uri) => {
+		console.log("uri.fsPath", uri.fsPath)
+		console.log('uri.path', uri.path)
+		console.log('uri.fragment', uri.fragment);
+		console.log('uri.scheme', uri);
+		const inputBox = vscode.window.createInputBox()
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from React Keeva!');
+		vscode.window.showInputBox(inputBox).then((componentName: string | undefined) => {
+			console.log(componentName);
+			if(componentName){
+				try {
+					
+					new Component(componentName, uri.fsPath).create()
+				} catch (error) {
+					console.log('====================================');
+					console.log(error);
+					console.log('====================================');
+				}
+			}
+		});
+		// vscode.window.showInformationMessage('Hello World from React Keeva!');
 	});
 
 	context.subscriptions.push(disposable);
